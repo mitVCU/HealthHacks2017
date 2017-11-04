0dd1dde98171c59df5007926c0ac84c6ac60fab6
mitvcu <aminm2@vcu.edu>
2017-11-04 19:19:16 -0400

    Initial commit

---
+++ app/src/main/java/com/mittens/healthhacks/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mittens.healthhacks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
