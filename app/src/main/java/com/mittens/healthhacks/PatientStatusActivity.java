74645ff63f54c25b6d63e017da2a8525c715c7e4
J. Zephyr Headley <headleyjz@vcu.edu>
2017-11-05 08:03:57 -0500

    graphs are setup to pull from the datbase.  They look decent enough.

--- app/src/main/java/com/mittens/healthhacks/PatientStatusActivity.java
+++ app/src/main/java/com/mittens/healthhacks/PatientStatusActivity.java
@@ -9,11 +9,17 @@ import com.google.firebase.database.ValueEventListener;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.mittens.healthhacks.models.SensorReading;
 
-import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class PatientStatusActivity extends AppCompatActivity {
     private static final String TAG = "PatientStatusActivity";
@@ -22,30 +28,89 @@ public class PatientStatusActivity extends AppCompatActivity {
     private GraphView bloodPressureGraph;
     private GraphView temperateGraph;
 
+    private ArrayList<DataPoint> heartRateData = new ArrayList<>();
+    private ArrayList<DataPoint> spo2Data = new ArrayList<>();
+    private ArrayList<DataPoint> bloodPressureData = new ArrayList<>();
+    private ArrayList<DataPoint> temperatureData = new ArrayList<>();
+
+
+    private TextView patientNameTv;
+    private TextView ageTv;
+    private double patientAge;
+    private String patientName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_status);
+        final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
         heartRateGraph = (GraphView) findViewById(R.id.heart_rate_graph);
         spo2Graph = (GraphView) findViewById(R.id.spo2_graph);
         bloodPressureGraph = (GraphView) findViewById(R.id.blood_pressure_graph);
         temperateGraph = (GraphView) findViewById(R.id.temperature_graph);
 
-        String patient = getIntent().getExtras().getString("patient");
+        patientNameTv = (TextView) findViewById(R.id.patient_name);
+        ageTv = (TextView) findViewById(R.id.patient_age);
+
+        final String patient = getIntent().getExtras().getString("patient");
         assert patient != null;
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("Clinic").child(patient);
         Log.d(TAG, "onCreate: " + ref);
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+        ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Log.d(TAG, "onDataChange: " + dataSnapshot);
-                Map<String, SensorReading> sensorReadings = (Map<String, SensorReading>) dataSnapshot;
+                patientName = patient;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Log.i(TAG, "onDataChange: " + snapshot.getValue());
+                    SensorReading reading = snapshot.getValue(SensorReading.class);
+                    patientAge = reading.getAge();
+                    try {
+                        heartRateData.add(new DataPoint(dateFormat.parse(reading.getTime()), reading.getHR()));
+                        spo2Data.add(new DataPoint(dateFormat.parse(reading.getTime()), reading.getSpO2()));
+                        bloodPressureData.add(new DataPoint(dateFormat.parse(reading.getTime()), reading.getSystolicBP()));
+                        temperatureData.add(new DataPoint(dateFormat.parse(reading.getTime()), reading.getTemperature()));
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
+                temperateGraph.addSeries(new LineGraphSeries<>(temperatureData.toArray(new DataPoint[] {})));
+                temperateGraph.setTitle("Temperature");
+                temperateGraph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getApplicationContext()));
+                temperateGraph.getGridLabelRenderer().setNumHorizontalLabels(3);
+                temperateGraph.getViewport().setMinY(50);
+                temperateGraph.getViewport().setMaxY(125);
+                temperateGraph.getViewport().setScalable(true);
+
+                spo2Graph.addSeries(new LineGraphSeries<>(spo2Data.toArray(new DataPoint[] {})));
+                spo2Graph.setTitle("SpO2");
+                spo2Graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getApplicationContext()));
+                spo2Graph.getGridLabelRenderer().setNumHorizontalLabels(3);
+                spo2Graph.getViewport().setMinY(50);
+                spo2Graph.getViewport().setMaxY(100);
+                spo2Graph.getViewport().setScalable(true);
+
+                bloodPressureGraph.addSeries(new LineGraphSeries<>(bloodPressureData.toArray(new DataPoint[] {})));
+                bloodPressureGraph.setTitle("Systolic Blood Pressure");
+                bloodPressureGraph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getApplicationContext()));
+                bloodPressureGraph.getGridLabelRenderer().setNumHorizontalLabels(3);
+                bloodPressureGraph.getViewport().setMinY(20);
+                bloodPressureGraph.getViewport().setMaxY(150);
+                bloodPressureGraph.getViewport().setScalable(true);
+
+                heartRateGraph.addSeries(new LineGraphSeries<>(heartRateData.toArray(new DataPoint[] {})));
+                heartRateGraph.setTitle("Heart Rate");
+                heartRateGraph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(getApplicationContext()));
+                heartRateGraph.getGridLabelRenderer().setNumHorizontalLabels(3);
+                heartRateGraph.getViewport().setMinY(0);
+                heartRateGraph.getViewport().setMaxY(300);
+                heartRateGraph.getViewport().setScalable(true);
 
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.e(TAG, "onCancelled: The hell happened here?");
             }
         });
     }
--- app/src/main/java/com/mittens/healthhacks/models/SensorReading.java
+++ app/src/main/java/com/mittens/healthhacks/models/SensorReading.java
@@ -1,4 +1,73 @@
 package com.mittens.healthhacks.models;
 
 public class SensorReading {
+    private double HR;
+    private double SpO2;
+    private double age;
+    private String sex;
+    private double systolicBP;
+    private double temperature;
+    private String time;
+
+    public SensorReading() {
+
+    }
+
+    public double getHR() {
+        return HR;
+    }
+
+    public void setHR(double HR) {
+        this.HR = HR;
+    }
+
+    public double getSpO2() {
+        return SpO2;
+    }
+
+    public void setSpO2(double spO2) {
+        SpO2 = spO2;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public double getSystolicBP() {
+        return systolicBP;
+    }
+
+    public void setSystolicBP(double systolicBP) {
+        this.systolicBP = systolicBP;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
 }
+
